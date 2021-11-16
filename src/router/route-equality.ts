a46cb91073d07ac69ce628557fa08dead9ee508b
electrovir <electrovir@users.noreply.github.com>
2021-11-16 06:31:26 -0700

    fix route equality checking

--- src/router/route-equality.ts
+++ src/router/route-equality.ts
@@ -1,13 +1,20 @@
 import type {FullRoute} from './full-route';
 
 export function areRoutesEqual(a: Readonly<FullRoute>, b: Readonly<FullRoute>): boolean {
-    if (a.paths.length !== b.paths.length) {
+    if (a.paths.join(' ') !== b.paths.join(' ')) {
         return false;
     }
 
-    return (
-        a.hash === b.hash &&
-        a.search === b.search &&
-        a.paths.every((entryA, index) => b.paths[index] === entryA)
-    );
+    if (typeof a.search === 'object' && typeof b.search === 'object') {
+        const aString: string = Object.entries(a.search).join(' ');
+        const bString: string = Object.entries(b.search).join(' ');
+
+        if (aString !== bString) {
+            return false;
+        }
+    } else if (a.search !== b.search) {
+        return false;
+    }
+
+    return a.hash === b.hash;
 }
